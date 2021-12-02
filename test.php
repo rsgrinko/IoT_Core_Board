392a16648fb69ff60e21a3b342579772bfb39e07
rsgrinko <rsgrinko@gmail.com>
2021-12-02 09:05:50 +0300

    add comments

--- test.php
+++ test.php
@@ -1,12 +1,11 @@
 <?php
-	require_once $_SERVER['DOCUMENT_ROOT'].'/inc/bootstrap.php';
-	if(!CUser::is_admin()) {
-		die('403 - Access denied');
-	}
-	
+/**
+ * Файл для тестирования и отладки нового функционала
+ */
+require_once $_SERVER['DOCUMENT_ROOT'].'/inc/bootstrap.php';
+if(!CUser::is_admin()) {
+	die('403 - Access denied');
+}
 
-	$result = getClientInfo();
-	
-	
-	
-	pre($result);
+$result = getClientInfo();
+pre($result);
