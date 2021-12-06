a1cc076f1381c3e750afce1f92c41fc3497a2f55
rsgrinko <rsgrinko@gmail.com>
2021-12-06 12:22:01 +0300

    разрешено админу смотреть инфо о контроллерах

--- boardinfo.php
+++ boardinfo.php
@@ -5,7 +5,7 @@
 		header("Location: index.php?"); die();
 	}
 	$deviceId = prepareString($_REQUEST['id']);
-	if(!isHaveAccessToDevice($deviceId, $USER['id'])) {
+	if(!isHaveAccessToDevice($deviceId, $USER['id']) and !CUser::is_admin()) {
 		die('403 - Access denied');
 	}
 	
