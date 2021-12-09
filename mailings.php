a3a97ac47ac1d456104b99273a68feacf9c25022
rsgrinko <rsgrinko@gmail.com>
2021-12-09 17:15:27 +0300

    [+] добавлена страница почтовых рассылок

---
+++ mailings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Страница управления потчовыми рассылками
+ */
+require_once __DIR__ . '/inc/bootstrap.php';
+require_once __DIR__ . '/inc/header.php';
+?>
+mailings
+<?php
+require_once __DIR__ . '/inc/footer.php';
+?>
