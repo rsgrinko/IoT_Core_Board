3c0006b329e3752f7489e3a921607aa8250aa361
rsgrinko <rsgrinko@gmail.com>
2021-12-21 22:27:21 +0300

    [+] добавлена поддержка аналогого порта A0

--- controller/handler.php
+++ controller/handler.php
@@ -1,49 +1,53 @@
 <?php
-/*
-	Обработчик запросов от контроллера
-	Данный файл входит в состав системы IoT Core System
-	Разработчик: Роман Сергеевич Гринько
-	E-mail: rsgrinko@gmail.com
-	Сайт: https://it-stories.ru
-*/
+/**
+ *    Обработчик запросов от контроллера
+ *    Данный файл входит в состав системы IoT Core System
+ *    Разработчик: Роман Сергеевич Гринько
+ *    E-mail: rsgrinko@gmail.com
+ *    Сайт: https://it-stories.ru
+ */
 require_once __DIR__ . '/../inc/bootstrap.php';
-if (!isset($_REQUEST['mac']) or $_REQUEST['mac'] == '') {     // если устройство не удалось идентифицировать - не продолжаем
+
+// если устройство не удалось идентифицировать - не продолжаем
+if (!isset($_REQUEST['mac']) or $_REQUEST['mac'] == '') {
     die('0000');
 }
 
+// для тестов логируем крайний запрос
 file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/controller/log.txt', print_r($_REQUEST, true));
 
+// предварительная обработка для защиты от SQL injection
 $DEVICE['mac'] = prepareString($_REQUEST['mac']);
 $DEVICE['chipid'] = prepareString($_REQUEST['chipid']);
 $DEVICE['hw'] = prepareString($_REQUEST['hw']);
 $DEVICE['fw'] = prepareString($_REQUEST['fw']);
 
-if (!CIoT::isDeviceExists($DEVICE['mac'])) {                     // если устройства нет в базе
-    CIoT::addDevice($DEVICE['mac'], $DEVICE['chipid'], $DEVICE['hw'], $DEVICE['fw']);  // добавляем его
+// если устройство не найдено в базе - регистрируем его
+if (!CIoT::isDeviceExists($DEVICE['mac'])) {
+    CIoT::addDevice($DEVICE['mac'], $DEVICE['chipid'], $DEVICE['hw'], $DEVICE['fw']);
 }
 
 $DEVICE['id'] = CIoT::getDeviceId($DEVICE['mac']);
 
+// обновление информации об устройстве
 CIoT::updateDeviceInfo($DEVICE['id'], $DEVICE['fw']);
 
-/**
- * Добавляем показания датчиков DS18B20 при наличии
- */
-if (isset($_REQUEST['ds']) and !empty($_REQUEST['ds'])) {            // если принята телеметрия с датчиков dallas
-    foreach ($_REQUEST['ds'] as $key => $dallasSensor) {            // забиваем показания температурных датчиков
+// добавляем показания датчиков DS18B20 при наличии
+if (isset($_REQUEST['ds']) and !empty($_REQUEST['ds'])) {
+    foreach ($_REQUEST['ds'] as $key => $dallasSensor) {
         CIoT::addDallasData($DEVICE['id'], 'ds' . ($key + 1), prepareString($dallasSensor));
     }
 }
 
-/**
- * Добавляем показания аналогого пина при наличии
- */
+// добавляем показания аналогого пина при наличии
 if (isset($_REQUEST['analog']) and !empty($_REQUEST['analog'])) {
     CIoT::addAnalogData($DEVICE['id'], prepareString($_REQUEST['analog']));
 }
 
+// получаем состояние каналов реле
 $DEVICE['relays'] = CIoT::getRelaysState($DEVICE['id']);
 
+// отдаем контроллеру требуемые состояния реле
 foreach ($DEVICE['relays'] as $relayState) {
     echo $relayState;
 }
