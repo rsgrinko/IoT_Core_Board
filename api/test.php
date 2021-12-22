23111406d3c09f24c64632fa362d5b471cc0f54f
rsgrinko <rsgrinko@gmail.com>
2021-12-22 17:17:23 +0300

    тест

--- api/test.php
+++ api/test.php
@@ -6,15 +6,16 @@
         echo CJson::create(['status' => 'fail', 'message' => 'Token not set']);
         die();
     }
-/*
-    if(!isset($_REQUEST['token']) or empty($_REQUEST['token'])) {
-        echo CJson::create(['status' => 'fail', 'message' => 'Token not found']);
-        die();
-    }*/
-
-
-
 
     $token = prepareString($_REQUEST['token']);
+
+    if(!CUser::isTokenExists($token)) {
+        echo CJson::create(['status' => 'fail', 'message' => 'Token not found']);
+        die();
+    }
+
     $USER = CUser::getUserByToken($token);
+
+    // имеем пользователя, который что то хочет. действуем...
+    // TODO: дописать реализацию API
     pre($USER);
