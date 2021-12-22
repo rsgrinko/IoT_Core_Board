3f6e77e3220dbefcdc0cc0856761f8eb57ef9fe5
rsgrinko <rsgrinko@gmail.com>
2021-12-22 17:13:21 +0300

    tset

--- api/test.php
+++ api/test.php
@@ -1,4 +1,20 @@
 <?php
 	require_once __DIR__ . '/../inc/bootstrap.php';
-    
-    echo generateGUID();
+    unset($USER);
+
+    if(!isset($_REQUEST['token']) or empty($_REQUEST['token'])) {
+        echo CJson::create(['status' => 'fail', 'message' => 'Token not set']);
+        die();
+    }
+/*
+    if(!isset($_REQUEST['token']) or empty($_REQUEST['token'])) {
+        echo CJson::create(['status' => 'fail', 'message' => 'Token not found']);
+        die();
+    }*/
+
+
+
+
+    $token = prepareString($_REQUEST['token']);
+    $USER = CUser::getUserByToken($token);
+    pre($USER);
