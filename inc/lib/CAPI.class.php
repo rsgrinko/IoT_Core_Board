bf4da864b9e196f55bac146a54b262d717bb6fc3
rsgrinko <rsgrinko@gmail.com>
2022-01-12 11:26:34 +0300

    add api method getUserInfo

--- inc/lib/CAPI.class.php
+++ inc/lib/CAPI.class.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * Класс для предоставления API функционала
+ */
 class CAPI {
 
-    public static function test(){
+    public static function test() {
         return ['status' => 'ok', 'message' => 'test was completed :)'];
+    }
+
+    public static function getUserInfo() {
+        global $USER;
+        unset($USER['pass']);
+        return $USER;
     }
 }
