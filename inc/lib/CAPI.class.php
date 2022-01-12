7b2c0c2f70335aefdbeadfe87e683bfd361271cf
rsgrinko <rsgrinko@gmail.com>
2022-01-12 12:36:31 +0300

    добавлены параметры устройств пользователя

--- inc/lib/CAPI.class.php
+++ inc/lib/CAPI.class.php
@@ -13,6 +13,9 @@ class CAPI {
         unset($USER['password']);
         $result = $USER;
         $result['devices'] = getUserDevices($USER['id']);
+        foreach($result['devices'] as $key => $value) {
+            $result['devices'][$key]['config'] = CIoT::getBoardConfig($value['id']);
+        }
         return $result;
     }
 }
