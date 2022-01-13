cf0b1c40e6233d0dab77833a16422726910eb48d
rsgrinko <rsgrinko@gmail.com>
2022-01-13 11:59:41 +0300

    Добавлен метод получение показаний датчика

--- inc/lib/CAPI.class.php
+++ inc/lib/CAPI.class.php
@@ -16,6 +16,22 @@ class CAPI {
         foreach($result['devices'] as $key => $value) {
             $result['devices'][$key]['config'] = CIoT::getBoardConfig($value['id']);
         }
+        $result['status'] = 'ok';
         return $result;
+    }
+
+    public static function getSensorValue() {
+        global $USER;
+        $sensor = $_REQUEST['sensor'];
+        $deviceId = $_REQUEST['deviceId'];
+
+        $arUserDevices = getUserDevices($USER['id']);
+        if (!in_array($deviceId, $arUserDevices)) {
+            return ['status' => 'fail', 'message' => 'Выбранное устройство не принадлежит текущему пользователю'];
+        }
+
+        $sensorValue = CIoT::getSensorData($deviceId, $sensor);
+        return ['status' => 'ok', 'deviceId' => $deviceId, 'sensor' => $sensor, 'value' => $sensorValue];
+       
     }
 }
